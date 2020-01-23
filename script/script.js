ad6dd48fcc664be4c4d9ebe2ade106a64e78295b
A. Bezrukov <a.bezrukov@gmail.com>
2020-01-23 22:27:16 +0300

    Первый коммит

---
+++ script/script.js
@@ -0,0 +1,20 @@
+'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+
+  var money = -10,
+      income = 1000;
+
+  alert('Hello, World!'+ ' ' +
+        'At the moment, I have no money. Please, send me' + ' ' +
+        income + '₽');
+  
+  const addExpenses = [10, 200, 50, 400],
+        deposit = 100;
+
+  let mission = money+income,
+      period = [100, 2234, 3000, [4-1], 'q'];
+
+  console.log(mission+deposit-addExpenses[1]);
+  console.log(period[4]);
+
+});
