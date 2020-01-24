a1c22e4a332aff40ba2f20a81582d410b36607bb
A. Bezrukov <a.bezrukov@gmail.com>
2020-01-24 22:35:29 +0300

    Урок 1 - Отработал замечания Олега

--- script/script.js
+++ script/script.js
@@ -5,25 +5,24 @@ document.addEventListener('DOMContentLoaded', () => {
       income = 1000;
 
   let message = function() {
-    alert('Привет!'+ ' ' +
+    alert('Привет!' + ' ' +
           'Изучил git и github. Готов к заработку' + ' ' +
           income + '₽');
   };
 
   document.getElementById('github').addEventListener('click', message);
 
-  
-  console.log('Hello, World!'+ ' ' +
+  console.log('Hello, World!' + ' ' +
         'At the moment, I have no money. Please, send me' + ' ' +
         income + '₽');
   
   const addExpenses = [10, 200, 50, 400],
         deposit = 100;
 
-  let mission = money+income,
-      period = [100, 2234, 3000, [4-1], 'q'];
+  let mission = money + income,
+      period = [100, 2234, 3000, [ 4 - 1 ], 'q'];
 
-  console.log(mission+deposit-addExpenses[1]);
+  console.log(mission + deposit - addExpenses[1]);
   console.log(period[4]);
-
+  
 });
