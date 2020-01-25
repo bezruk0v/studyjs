3540160c32a70bb25543f353caef2fa12f8367bd
A. Bezrukov <a.bezrukov@gmail.com>
2020-01-25 16:12:10 +0300

    Урок 2: Усложненное задание

--- script/script.js
+++ script/script.js
@@ -15,4 +15,13 @@ document.addEventListener('DOMContentLoaded', () => {
       console.log(addExpenses.toLowerCase().split(', '));
       console.log(budgetDay);
 
+      const num = 266219;
+
+      console.log((num.toString().match(/.{1}/g).reduce((a, b) => a * b) ** 3).toString().substring(0, 2));
+
+      const numMultiply = num.toString().match(/.{1}/g).reduce((a, b) => a * b);
+
+      console.log(numMultiply ** 3);
+      console.log((numMultiply ** 3).toString().substring(0, 2));
+      
 });
