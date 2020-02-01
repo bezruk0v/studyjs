e665c3a35454b5f892b6ab44d6962a02c33dc68c
A. Bezrukov <a.bezrukov@gmail.com>
2020-02-01 19:20:38 +0300

    Урок 3: Обязательное задание

--- script/script.js
+++ script/script.js
@@ -1,20 +1,41 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', () => {
 
-      const money = 15000,
-            income = 'Freelance',
-            addExpenses = 'Taxi, Products, Communal payments, Internet, Hosting, Taxes',
-            deposit = 5 !== 1,
-            mission = 100500,
-            period = 5,
-            budgetDay = money / 30;
+      let money = prompt('Ваш месячный доход?', '10000');
+      let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую',
+            'Машина, Коммунальные платежи, Продукты, Интернет, Сотовая связь, Хостинг');
+      let deposit = confirm('Есть ли у вас депозит в банке?', 'Да', 'Нет');
+      let income = 'Freelance';
+      let mission = 100500;
+      let period = 5;
 
-      console.log('Тип данных переменной "money": ',typeof money);
-      console.log('Тип данных переменной "income": ',typeof income);
-      console.log('Тип данных переменной "deposit": ',typeof deposit);
-      console.log('Длина строки "addExpenses": ',addExpenses.length);
+      console.log('Ваш месячный доход, ₽?: ', typeof money);
+      console.log('Тип данных переменной "income": ', typeof income);
+      console.log('Есть ли у вас депозит в банке?: ', typeof deposit);
+      console.log('Длина строки "addExpenses": ', addExpenses.length);
       console.log(`Цель заработать ${mission * period}₽` + ' ' + `за период равный ${period} месяцам`);
-      console.log('Значения строки "addExpenses" к нижнему регистру и разбиты на массив: ',addExpenses.toLowerCase().split(', '));
-      console.log('Дневной бюджет (доход за месяц / 30): ',budgetDay);
+
+      let expenses1 = prompt('Введите обязательную статью расходов?', 'Машина');
+      let amount1 = prompt('Во сколько это обойдется?', '1000');
+      let expenses2 = prompt('Введите ещё одну обязательную статью расходов?', 'Коммунальные платежи');
+      let amount2 = prompt('Во сколько это обойдется?', '2000');
+
+      let budgetMonth = money - amount1 - amount2;
+      let budgetDay = Math.floor(budgetMonth / 30);
+
+      console.log('Возможные расходы за рассчитываемый период: ', addExpenses.toLowerCase().split(', '));
+      console.log('Бюджет на месяц (доходы - расходы), ₽: ', budgetMonth);
+      console.log('Цель', `${mission}₽`, 'будет достигнута за: ', Math.ceil(mission / budgetMonth), ' мес.');
+      console.log('Дневной бюджет (доход за месяц / 30): ', budgetDay);
+
+      if (budgetDay > 1200) {
+            console.log('У вас высокий уровень дохода!');
+      } else if (budgetDay >= 600 && budgetDay <= 1200) {
+            console.log('У вас средний уровень дохода.');
+      } else if (budgetDay < 600 && budgetDay >= 0) {
+            console.log('К сожалению, у вас уровень дохода ниже среднего...');
+      } else {
+            console.log('Что то пошло не так!');
+      }
 
 });
