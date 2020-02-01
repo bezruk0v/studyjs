ad17bf857c8b963a1c1b0c506f86277138eaf14c
A. Bezrukov <a.bezrukov@gmail.com>
2020-02-01 22:54:16 +0300

    Урок 3: Усложненное задание (подчистил)

--- script/script.js
+++ script/script.js
@@ -1,11 +1,50 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', () => {
 
-      const num = 266219;
-      const numMultiply = num.toString().match(/.{1}/g).reduce((a, b) => a * b);
+  const ru = 'Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье';
+  const en = 'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday';
 
-      console.log(`Произведение цифр числа ${num}: `, numMultiply);
-      console.log('Возвёл число в степень 3 с помощью оператора "**": ',(numMultiply ** 3));
-      console.log('Первые две цифры: ',(numMultiply ** 3).toString().substring(0, 2));
-      
+  const lang = confirm('Выберите язык: ОК (Русский), Отмена (Английский)');
+
+  // Решение задачи через if
+  if (lang === true) {
+    console.log('Дни недели: ', ru.split(', '));
+  } else if (lang === false) {
+    console.log('Days of week: ', en.split(', '));
+  }
+
+  // Решение задачи через switch-case
+  switch (lang) {
+    case true:
+      console.log('Дни недели: ', ru.split(', '));
+      break;
+    case false:
+      console.log('Days of week: ', en.split(', '));
+      break;
+    default:
+      console.log('Что-то пошло не так!');
+  }
+
+  // Решение задачи через многомерный массив без if & switch
+  const langArr = [
+    [ru.split(', ')],
+    [en.split(', ')]
+  ];
+  console.log('Дни недели: ', langArr[0]);
+  console.log('Days of week: ', langArr[1]);
+
+  // Решение задачи с помощью нескольких тернарных операторов, без использования if или switch
+  const namesAndJobs = [
+    ['Артем', 'Максим'],
+    ['Директор', 'Преподаватель', 'Студент']
+  ];
+
+  let namePerson = prompt('Введите имя', 'Артем');
+
+  let jobCheck =
+    (namePerson === namesAndJobs[0][0]) ? namesAndJobs[1][0] :
+    (namePerson === namesAndJobs[0][1]) ? namesAndJobs[1][1] : namesAndJobs[1][2];
+
+  console.log('Должность, которая соответствует имени', `${namePerson}:`, jobCheck);
+
 });
