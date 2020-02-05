6de444609f704d95cddd0a45128dba0abcf7be4c
A. Bezrukov <a.bezrukov@gmail.com>
2020-02-05 16:28:27 +0300

    Урок 6: Обязательное задание (переписал код, но остался вопрос по confirm)

--- script/script.js
+++ script/script.js
@@ -1,20 +1,21 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', () => {
 
-let botData = {
-    userNumber: +prompt('Угадай число от 1 до 100'),
-    randNum: Math.floor(Math.random() * 100) + 1
-};
-    console.log(botData.randNum);
-
     const checkResult = () => {
-        console.log(botData.userNumber);
-        if (botData.userNumber === botData.randNum) {
+        let randNum = Math.floor(Math.random() * 100) + 1;
+        console.log(randNum);
+
+        const userNumber = +prompt('Угадай число от 1 до 100');
+        console.log(userNumber);
+        if (userNumber === randNum) {
             alert('Ура, вы угадали!');
-        } else if (botData.userNumber < botData.randNum) {
+            confirm('Повторишь?');
+        } else if (userNumber < randNum) {
             alert('Загаданное число меньше');
-        } else if (botData.userNumber > botData.randNum) {
+            checkResult();
+        } else if (userNumber > randNum) {
             alert('Загаданное число больше');
+            checkResult();
         }
     };
     checkResult();
